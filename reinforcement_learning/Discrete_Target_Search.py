c981caa2cd7b86dec86eb7aacecbf0d3bf67316e
Anthony Dowling <dowlinah@clarkson.edu>
2022-04-29 12:44:40 -0400

    update env to have new actions

--- reinforcement_learning/Discrete_Target_Search.py
+++ reinforcement_learning/Discrete_Target_Search.py
@@ -11,6 +11,7 @@ from gym.utils import seeding
 from tf_agents.environments import py_environment
 from tf_agents.trajectories import time_step as ts
 from tf_agents.specs import array_spec
+from numba import njit, prange
 from EnvGenerator import RoomGenerator
 
 from Test_Environment import TargetSearchEnv
@@ -18,7 +19,7 @@ from Test_Environment import TargetSearchEnv
 class DiscreteTargetSearchEnv(py_environment.PyEnvironment):
     def __init__(self):
         self._env = TargetSearchEnv()
-        self._action_spec = array_spec.BoundedArraySpec(shape=(), dtype=np.int32,minimum=0,maximum=2,name='action')
+        self._action_spec = array_spec.BoundedArraySpec(shape=(), dtype=np.int32,minimum=0,maximum=4,name='action')
         
     def action_spec(self):
         return self._action_spec
@@ -30,9 +31,11 @@ class DiscreteTargetSearchEnv(py_environment.PyEnvironment):
     
     def _step(self,action):
         action_map = {
-            0 : [0,-1],
-            1 : [1,0],
-            2 : [0,1]
+            0 : [0.1,-1],
+            1 : [0.5,-0.5],
+            2 : [1,0],
+            3 : [0.5,0.5],
+            4 : [0.1,1]
         }
         return self._env._step(action_map[int(action)],prev_action=int(action))
     
